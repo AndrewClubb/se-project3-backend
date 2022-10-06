77df2bf186de85ccdd0f5aa9031c16858588448f
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2022-10-06 12:47:22 -0500

    added controller files

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -8,16 +8,10 @@ module.exports = app => {
     router.get("/", course.findAllCourses);
     // Retrieve a single course with id
     router.get("/id/:id", course.findCourseById);
-    // Retrieve courses by dept
-    router.get("/dept/:dept", course.findCoursesByDept);
-    // Retrieve courses by name
-    router.get("/name/:name", course.findCoursesByName);
-    // Retrieve courses by course number
-    router.get("/num/:num", course.findCoursesByNumber);
     // Update a course with id
     router.put("/:id", course.update);
     // Delete a course with id
     router.delete("/:id", course.delete);
 
-    app.use('/schedule-t2', router);
+    app.use('/schedule-t2/course', router);
 };
