6fb503227d708a509e5d0cc7d6e62c51f7e28776
Alexandria Adams <alexm.adams@yahoo.com>
2022-10-19 16:52:41 -0500

    FIxed validation requests

--- controllers/faculty.controller.js
+++ controllers/faculty.controller.js
@@ -5,38 +5,33 @@ const Faculty = db.faculty;
 // Create and Save a new faculty
 exports.create = (req, res) => {
   // Validate request
-  if (!req.body.title) {
+  if (!req.body.fName) {
     res.status(400).send({
-      message: "Title cannot be empty!"
-    });
-    return;
-  } else if (!req.body.fName) {
-    res.status(400).send({
-      message: "First name cannot be empty!"
+      message: "First name cannot be empty!",
     });
     return;
   } else if (!req.body.lName) {
     res.status(400).send({
-      message: "Last name cannot be empty!"
+      message: "Last name cannot be empty!",
     });
     return;
   }
-  
+
   const faculty = {
     title: req.body.title,
     fName: req.body.fName,
-    lName: req.body.lName
+    lName: req.body.lName,
   };
 
   // Create and Save a new faculty
   Faculty.create(faculty)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the faculty."
+          err.message || "Some error occurred while creating the faculty.",
       });
     });
 };
@@ -44,13 +39,12 @@ exports.create = (req, res) => {
 // Retrieve all faculty from the database
 exports.findAll = (req, res) => {
   Faculty.findAll()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving faculty."
+        message: err.message || "Some error occurred while retrieving faculty.",
       });
     });
 };
@@ -59,18 +53,18 @@ exports.findAll = (req, res) => {
 exports.findById = (req, res) => {
   const id = req.params.id;
   Faculty.findByPk(id)
-    .then(data => {
+    .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: 'Cannot find faculty with id=' + id
+          message: "Cannot find faculty with id=" + id,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving faculty with id=' + id
+        message: "Error retrieving faculty with id=" + id,
       });
     });
 };
@@ -79,41 +73,46 @@ exports.findById = (req, res) => {
 exports.update = (req, res) => {
   const id = req.params.id;
   Faculty.update(req.body, {
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'Faculty was updated successfully.'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Faculty was updated successfully.",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot update faculty with id=" +
+            id +
+            ". Maybe faculty was not found or req.body is empty!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating faculty with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot update faculty with id=' + id + '. Maybe faculty was not found or req.body is empty!'
-      });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: 'Error updating faculty with id=' + id
     });
-  });
 };
 
 // Delete a faculty with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
   Faculty.destroy({
-    where: { id: id }
-  })
-  .then(num => {
+    where: { id: id },
+  }).then((num) => {
     if (num == 1) {
       res.send({
-        message: 'Faculty was deleted successfully!'
+        message: "Faculty was deleted successfully!",
       });
     } else {
       res.send({
-        message: 'Cannot delete faculty with id=' + id + '. Maybe faculty was not found'
-      })
+        message:
+          "Cannot delete faculty with id=" +
+          id +
+          ". Maybe faculty was not found",
+      });
     }
-  })
+  });
 };
--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -7,30 +7,24 @@ exports.create = (req, res) => {
   // Validate request
   if (!req.body.number) {
     res.status(400).send({
-      message: "Number can not be empty!"
-    });
-    return;
-  } else if (!req.body.capacity) {
-    res.status(400).send({
-      message: "Capacity can not be empty!"
+      message: "Number can not be empty!",
     });
     return;
   }
-  
+
   const room = {
     number: req.body.number,
-    capacity: req.body.capacity
+    capacity: req.body.capacity,
   };
 
   // Create and Save a new room
   Room.create(room)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the room."
+        message: err.message || "Some error occurred while creating the room.",
       });
     });
 };
@@ -38,13 +32,12 @@ exports.create = (req, res) => {
 // Retrieve all rooms from the database
 exports.findAll = (req, res) => {
   Room.findAll()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving rooms."
+        message: err.message || "Some error occurred while retrieving rooms.",
       });
     });
 };
@@ -53,18 +46,18 @@ exports.findAll = (req, res) => {
 exports.findById = (req, res) => {
   const id = req.params.id;
   Room.findByPk(id)
-    .then(data => {
+    .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: 'Cannot find room with id=' + id
+          message: "Cannot find room with id=" + id,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving room with id=' + id
+        message: "Error retrieving room with id=" + id,
       });
     });
 };
@@ -73,41 +66,44 @@ exports.findById = (req, res) => {
 exports.update = (req, res) => {
   const id = req.params.id;
   Room.update(req.body, {
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'Room was updated successfully.'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Room was updated successfully.",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot update room with id=" +
+            id +
+            ". Maybe room was not found or req.body is empty!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating room with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot update room with id=' + id + '. Maybe room was not found or req.body is empty!'
-      });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: 'Error updating room with id=' + id
     });
-  });
 };
 
 // Delete a room with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
   Room.destroy({
-    where: { id: id }
-  })
-  .then(num => {
+    where: { id: id },
+  }).then((num) => {
     if (num == 1) {
       res.send({
-        message: 'Room was deleted successfully!'
+        message: "Room was deleted successfully!",
       });
     } else {
       res.send({
-        message: 'Cannot delete room with id=' + id + '. Maybe room was not found'
-      })
+        message:
+          "Cannot delete room with id=" + id + ". Maybe room was not found",
+      });
     }
-  })
+  });
 };
