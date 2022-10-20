b17e29e4312002d23bc0655b018ccec1dda55c38
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2022-10-19 19:19:33 -0500

    changed csv parsing module

--- controllers/csv.controller.js
+++ controllers/csv.controller.js
@@ -1,6 +1,6 @@
 const db = require("../models");
-const fs = require("fs");
-const csv = require("fast-csv");
+var fs = require('fs').promises;
+const csv = require("csv-parse/sync");
 
 const Faculty = db.faculty;
 const Section = db.section;
@@ -11,61 +11,43 @@ const Semester = db.semester;
 let facultyDatabase = [], sectionDatabase = [], roomDatabase = [], courseDatabase = [], semesterDatabase = [];
 
 exports.upload = async (req, res) => {
+  if (req.file == undefined) {
+    return res.status(400).send("Please upload a CSV file!");
+  }
+
   try {
-    if (req.file == undefined) {
-      return res.status(400).send("Please upload a CSV file!");
-    }
-
-    let path = "resources/static/assets/uploads/" + req.file.filename;
-
-    //fill arrays with data from database
     await loadData();
 
-    fs.createReadStream(path)
-      .pipe(csv.parse({ headers: true }))
-      .on("error", (error) => {
-        throw error.message;
-      })
-      .on("data", (row) => {
-        //Faculty
-        const facultyId = findFacultyId(row);
+    let fileContent = await fs.readFile("resources/static/assets/uploads/" + req.file.filename);
+    const records = csv.parse(fileContent, {columns: true});
 
-        //Course?
+    for(let index = 0; index < records.length; index++) {
+      let row = records[index];
 
-        //Semester?
+      //Faculty
+      const facultyId = await findFacultyId(row["Faculty Name (LFM)"]);
 
-        //Room
+      //Course?
 
-        //Section
+      //Semester?
 
-        //FacultySection
+      //Room
 
-        //SectionTimes
-      })
-      .on("end", () => {
-        // Tutorial.bulkCreate(tutorials)
-        //   .then(() => {
-        //     res.status(200).send({
-        //       message:
-        //         "Uploaded the file successfully: " + req.file.originalname,
-        //     });
-        //   })
-        //   .catch((error) => {
-        //     res.status(500).send({
-        //       message: "Fail to import data into database!",
-        //       error: error.message,
-        //     });
-        //   });
+      //Section
 
-        res.status(200).send({
-          message:
-            "Uploaded the file successfully: " + req.file.originalname,
-        });
-      });
+      //FacultySection
+
+      //SectionTimes
+    }
+
+    res.status(200).send({
+      message:
+        "Uploaded the file successfully: " + req.file.originalname,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
-      message: "Could not upload the file: " + req.file.originalname,
+      message: "There was a problem uploading the file: " + req.file.originalname,
     });
   }
 };
@@ -123,12 +105,26 @@ async function loadData() {
     });
 };
 
-function findFacultyId(row) {
-  var id;
-  var rowFName = row.fName;
-  var rowLName = row.lName;
+async function findFacultyId(input) {
+  var lName = input.substring(0,input.indexOf(","));
+  var fName = input.substring(input.indexOf(",") + 2, input.lastIndexOf(" "));
+  var tempFaculty = facultyDatabase.find(fac => fac.fName === fName && fac.lName === lName);
 
-  console.log(row);
+  if(tempFaculty === null || tempFaculty === undefined) {
+    const faculty = {
+      fName: fName,
+      lName: lName
+    };
 
-  return id;
+    await Faculty.create(faculty)
+    .then(data => {
+      tempFaculty = data.dataValues;
+      facultyDatabase.push(tempFaculty);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  
+  return tempFaculty.id;
 };
