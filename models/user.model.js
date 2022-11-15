09cbc2bb8424f0bfe79d738f3831697b00f7ae5d
Alexandria Adams <alexm.adams@yahoo.com>
2022-11-14 23:49:14 -0600

    Making role work so google auth can work

--- models/user.model.js
+++ models/user.model.js
@@ -1,23 +1,34 @@
 module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define("user", {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      unique: true
+  const User = sequelize.define(
+    "user",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+      },
+      fName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
     },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    role: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-  },
-  {
-    timestamps: false
-  });
+    {
+      timestamps: false,
+    }
+  );
 
   return User;
 };
